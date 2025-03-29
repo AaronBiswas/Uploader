cba29fc815b8c2db238ca5c8c627c23a1e11a58b
Aaron Biswas <savybiswas15@gmail.com>
2025-03-29 15:37:29 +0530

    rebuild dist and redirected the page to UI

--- Backend/Backend/urls.py
+++ Backend/Backend/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', RedirectView.as_view(url='/api/', permanent=True)),
+    path('', TemplateView.as_view(template_name='index.html')),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
